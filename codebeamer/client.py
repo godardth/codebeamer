7d0218f9482f4b393cc736cc223193e2d2ca8b1b
GodardTheophile/선임연구원/JLR P-IVI AVN Project(theophile.godard@lge.com) <theophile.godard@lge.com>
2020-01-15 16:54:54 +0900

    Add requirements for PyPI deployment

---
+++ codebeamer/client.py
@@ -0,0 +1,35 @@
+import requests, json
+
+
+class Codebeamer(object):
+
+    def __init__(self, url, login, password):
+        self.base_url = url
+        self.auth = (login, password)
+
+    def get(self, uri):
+        url = self.base_url + uri
+        res = requests.get(url, auth=self.auth, verify=True)
+        if res.status_code == 200:
+            return json.loads(res.content)
+        else:
+            print(f"Warning : GET error ({url})")
+            return None
+
+    def put(self, uri, data):
+        url = self.base_url + uri
+        res = requests.put(url, json=data, auth=self.auth, verify=True)
+        if res.status_code == 200:
+            return json.loads(res.content)
+        else:
+            print(f"Warning : PUT error ({url})")
+            return None
+
+    def post(self, uri, data):
+        url = self.base_url + uri
+        res = requests.post(url, json=data, auth=self.auth, verify=True)
+        if res.status_code == 201:
+            return json.loads(res.content)
+        else:
+            print(f"Warning : POST error ({url})")
+            return None
---
+++ setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="codebeamer-client",
+    version="0.0.1",
+    description="Client for the CodeBeamer REST API",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/godardth/codebeamer",
+    author="Theophile Godard",
+    author_email="theo.godard@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=["codebeamer"],
+    include_package_data=True,
+)
